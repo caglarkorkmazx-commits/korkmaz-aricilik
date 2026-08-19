582c77ff7604cd5f390437fd468675de9f50cc62
caglarkorkmazx-commits <caglarkorkmazx@gmail.com>
2026-08-19 19:19:14 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', textAlign: 'center' }}>
+      <h1 style={{ color: '#f59e0b' }}>Korkmaz Arıcılık</h1>
+      <p>Sitemiz hazırlanıyor, çok yakında yayındayız!</p>
+    </main>
+  )
+}
