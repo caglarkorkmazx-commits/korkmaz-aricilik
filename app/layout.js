6ad493d2e69b05b52089fe9fe4f8b016add06f90
caglarkorkmazx-commits <caglarkorkmazx@gmail.com>
2026-09-03 22:41:20 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Header from '../components/Header'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Korkmaz Arıcılık - Belfast Ana Arı & Bal Satışı',
@@ -8,6 +9,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-F3EVRQ8045"
+        />
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F3EVRQ8045');
+          `}
+        </Script>
+      </head>
       <body style={{ margin: 0, padding: 0, backgroundColor: '#121212', color: '#ffffff', fontFamily: 'sans-serif' }}>
         {/* Header bileşenini en üste koyuyoruz */}
         <Header />
